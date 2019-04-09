9c787ab2bd2a255d0e46ab374cbf181888d638da
vadimeladii <vadimeladii@gmail.com>
2019-04-09 23:18:18 +0300

    Non Primitive Data Types

---
+++ basic/src/com/mentoring/variable/NonPrimitiveDataType.java
@@ -0,0 +1,26 @@
+package com.mentoring.variable;
+
+public class NonPrimitiveDataType {
+
+//    byte     -> Byte
+//    short    -> Short
+//    int      -> Integer
+//    long     -> Long
+//    float    -> Floating
+//    double   -> Double
+//    char     -> Character
+//    boolean  -> Boolean
+//    String   -> String
+
+//    type variable = value;
+
+    Byte myByteNum = 7;
+    Short myShortNum = 347;
+    Integer myIntNum = 50045;
+    Long myLongNum = 7843487438349437487L;
+    Float myFloatNum = 5.99F;
+    Double myDoubleNum = 4.43D;
+    Character myLetter = 'D';
+    Boolean myBool = true;
+    String myString = "Test";
+}
