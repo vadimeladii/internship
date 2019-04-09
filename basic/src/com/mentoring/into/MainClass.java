6f3c20b343940c5e2b7fd6902babfbd33d918e68
Vadim Eladii <veladii@straighterline.com>
2019-04-09 14:42:48 +0300

    Init Project

---
+++ basic/src/com/mentoring/into/MainClass.java
@@ -0,0 +1,15 @@
+package com.mentoring.into;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+//        java 8
+//        Arrays.stream(args).forEach(System.out::println);
+    }
+}
