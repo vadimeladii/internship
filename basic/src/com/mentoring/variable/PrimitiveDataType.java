16902289c93c9ef25027afe2100d8848a22edc0c
vadimeladii <vadimeladii@gmail.com>
2019-04-09 22:10:50 +0300

    Primitive data types

---
+++ basic/src/com/mentoring/variable/PrimitiveDataType.java
@@ -0,0 +1,26 @@
+package com.mentoring.variable;
+
+public class PrimitiveDataType {
+
+//    Primitive data types - includes byte, short, int, long, float, double, boolean and char
+
+//    byte	    1 byte	Stores whole numbers from       -128 to 127
+//    short	    2 bytes	Stores whole numbers from       -32,768 to 32,767
+//    int	    4 bytes	Stores whole numbers from       -2,147,483,648 to 2,147,483,647
+//    long	    8 bytes	Stores whole numbers from       -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//    float	    4 bytes	Stores fractional numbers from   3.4e−038 to 3.4e+038
+//    double	8 bytes	Stores fractional numbers from   1.7e−308 to 1.7e+038
+//    boolean	1 byte	Stores true or false values
+//    char	    2 bytes	Stores a single character/letter
+
+//    type variable = value;
+
+    byte myByteNum = 7;
+    short myShortNum = 347;
+    int myIntNum = 50045;
+    long myLongNum = 7843487438349437487L;
+    float myFloatNum = 5.99F;
+    double myDoubleNum = 4.43D;
+    boolean myBool = true;
+    char myLetter = 'D';
+}
