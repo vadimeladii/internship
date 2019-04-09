2e21fe1dd34fc0437d389fb1efd561d7038e7d32
vadimeladii <vadimeladii@gmail.com>
2019-04-09 15:03:03 +0300

    Java Comments

---
+++ basic/src/com/mentoring/comment/MainClass.java
@@ -0,0 +1,15 @@
+package com.mentoring.comment;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+//        This is a comment
+        System.out.println("Hello World");  // comment
+
+/*
+          The code below will print the words Hello World
+          to the screen, and it is amazing
+*/
+        System.out.println("Hello World");
+    }
+}
