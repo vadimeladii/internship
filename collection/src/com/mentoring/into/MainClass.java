0952f96fcb56523c30628c6f79c2fc407c4bc0ca
vadimeladii <vadimeladii@gmail.com>
2019-04-25 13:28:01 +0300

    Intraduction to Collection

---
+++ collection/src/com/mentoring/into/MainClass.java
@@ -0,0 +1,38 @@
+package com.mentoring.into;
+
+public class MainClass {
+
+/*
+    https://www.google.com/search?q=java+collection&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi0h-iy0unhAhUQ16QKHcWwALEQ_AUIDigB&biw=1920&bih=952#imgrc=IFg-xihuuUWOtM:
+
+    The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.
+
+    Java Collections can achieve all the operations that you perform on a data such as searching, sorting, insertion, manipulation, and deletion.
+
+    Iterable -> Iterator<T> iterator()
+
+    Collection -> add, addAll, clear, size, isEmpty, remove, removeAll, etc.
+*/
+
+    public static void main(String[] args) {
+/*
+        List
+
+        List <data-type> list1= new ArrayList();
+        List <data-type> list2 = new LinkedList();
+        List <data-type> list3 = new Vector();
+        List <data-type> list4 = new Stack();
+
+        Queue
+
+        Queue<String> q1 = new PriorityQueue();
+        Queue<String> q2 = new ArrayDeque();
+
+        Set
+
+        Set<data-type> s1 = new HashSet<data-type>();
+        Set<data-type> s2 = new LinkedHashSet<data-type>();
+        Set<data-type> s3 = new TreeSet<data-type>();
+*/
+    }
+}
